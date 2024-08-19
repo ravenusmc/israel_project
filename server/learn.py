eaa71f5f3789a9c267b351b0669faae1b53d464c
ravenusmc <mcuddy77@gmail.com>
2024-08-19 16:54:36 -0400

    set up navbar and started working on a data file

---
+++ server/learn.py
@@ -0,0 +1,17 @@
+# This file will be dealing with the CSV file to get ideas from it on what graphs I want to build. 
+
+# importing supporting libraries
+import numpy as np
+import pandas as pd
+
+class UnderstandCSV():
+
+    def __init__(self):
+        self.data = pd.read_csv('./data/fatalities_isr_pse_conflict_2000_to_2023.csv')
+
+    def columns(self):
+        print(self.data.head())
+
+
+obj = UnderstandCSV()
+obj.columns()
