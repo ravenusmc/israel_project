269176f850923cedef6dbc36a77d2c5a510141fb
ravenusmc <mcuddy77@gmail.com>
2024-08-22 16:49:17 -0400

    starting work on analyzing data set

--- server/data.py
+++ server/data.py
@@ -2,3 +2,11 @@
 import numpy as np
 import pandas as pd
 
+class ExamineData():
+
+    def __init__(self):
+        self.data = pd.read_csv('./data/fatalities_isr_pse_conflict_2000_to_2023.csv.csv')
+
+
+    def common_age_deaths(self): 
+        
