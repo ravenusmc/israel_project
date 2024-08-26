1a008732e4f9f1ab4ba62df06142bbeb9a996c9e
ravenusmc <mcuddy77@gmail.com>
2024-08-26 17:04:45 -0400

    working on average age of death graph

--- server/data.py
+++ server/data.py
@@ -7,8 +7,17 @@ import pandas as pd
 class ExamineData():
 
     def __init__(self):
-        self.data = pd.read_csv('./data/fatalities_isr_pse_conflict_2000_to_2023.csv.csv')
+        self.data = pd.read_csv('./data/fatalities_isr_pse_conflict_2000_to_2023.csv')
+        # Convert date_of_death to datetime format
+        self.data['date_of_death'] = pd.to_datetime(self.data['date_of_death'])
 
-    # Maybe allow to allow the user to select the year?
-    def average_age_deaths(self): 
-        pass 
+    def average_age_deaths(self, year):
+        # Filter data for deaths up to the given year
+        df_filtered = self.data[self.data['date_of_death'].dt.year <= year]
+        # Group by citizenship and calculate the average age
+        avg_age_by_citizenship = df_filtered.groupby('citizenship')['age'].mean()
+        return avg_age_by_citizenship
+
+death_dataset = ExamineData()
+print(death_dataset.average_age_deaths(2010))
+#Should look at average age overtime. 
