711c4b7afdff565189d1c8f9aa3153ca10a76469
ravenusmc <mcuddy77@gmail.com>
2024-09-18 16:29:44 -0400

    working on mid section of landing page

--- client/src/components/home/Midsection.vue
+++ client/src/components/home/Midsection.vue
@@ -1,6 +1,13 @@
 <template>
     <div>
-        hi
+      <div class="pic-row">
+        <div class="left-image">
+        </div>
+        <div>
+        </div>
+        <div class="'right-image'">
+        </div>
+      </div>
     </div>
 </template>
 
@@ -11,4 +18,23 @@ export default {
 </script>
 
 <style scoped>
+
+.pic-row {
+  display: grid; 
+  grid-template-columns: 1fr 1fr 1fr;
+
+}
+
+.left-image {
+  margin-top: 28px;
+  height: 60vh;
+  background: url("../../assets/images/Israel_flag.jpg") center/contain no-repeat;
+  background-position: center center;
+  background-size: 65%; /* Slightly increases the width */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+
 </style>
