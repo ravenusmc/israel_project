7a472f2a3dad79ce8b2d89b3909dc5471b1c1863
ravenusmc <mcuddy77@gmail.com>
2024-10-14 14:35:15 -0400

    working on setting up first graph

---
+++ client/src/components/graphs/Avgage.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+    <div id="AverageAge">Avg</div>
+    </div>
+</template>
+
+<script>
+import * as d3 from "d3";
+
+export default {
+    name: "Avgage",
+}
+
+</script>
+
+<style scoped>
+</style>
--- client/src/views/Data.vue
+++ client/src/views/Data.vue
@@ -1,10 +1,23 @@
 <template>
     <div>
-        Data
+        <h2>Examining The Data</h2>
+
+        <div class="graph-area">
+            <Avgage />
+        </div>
+
     </div>
 </template>
 
 <script>
+import Avgage from '@/components/graphs/Avgage.vue'
+
+export default {
+	name: "Missing",
+	components: {
+		Avgage,
+  },
+};
 
 </script>
 
